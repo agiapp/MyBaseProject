a450759ba00a16791f6750eb08770ddd0f486b7f
任波 <borenfocus@gmail.com>
2017-03-28 22:50:16 +0800

    添加UIImage+BRAdd分类

---
+++ MyBaseProject/MyBaseProject/Classes/Helper/Category/UIImage+BRAdd.h
@@ -0,0 +1,13 @@
+//
+//  UIImage+BRAdd.h
+//  MyBaseProject
+//
+//  Created by 任波 on 17/3/28.
+//  Copyright © 2017年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImage (BRAdd)
+
+@end
---
+++ MyBaseProject/MyBaseProject/Classes/Helper/Category/UIImage+BRAdd.m
@@ -0,0 +1,13 @@
+//
+//  UIImage+BRAdd.m
+//  MyBaseProject
+//
+//  Created by 任波 on 17/3/28.
+//  Copyright © 2017年 apple. All rights reserved.
+//
+
+#import "UIImage+BRAdd.h"
+
+@implementation UIImage (BRAdd)
+
+@end
