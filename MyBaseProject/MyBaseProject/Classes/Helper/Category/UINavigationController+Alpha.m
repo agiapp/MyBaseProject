b634050aaa20d3258af55e9b2ce34f5eb47fed48
任波 <borenfocus@gmail.com>
2017-03-29 23:07:29 +0800

    注释中UINavigationController+Alpha中的实现方法

--- MyBaseProject/MyBaseProject/Classes/Helper/Category/UINavigationController+Alpha.m
+++ MyBaseProject/MyBaseProject/Classes/Helper/Category/UINavigationController+Alpha.m
@@ -12,53 +12,53 @@
 #define SYSTEM_VERSION_LESS_THAN(v) ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedAscending)
 
 @implementation UINavigationController (Alpha)
-static char overlayKey;
+//static char overlayKey;
 
-- (UIView *)overlay {
-    return objc_getAssociatedObject(self, &overlayKey);
-}
-
-- (void)setOverlay:(UIView *)overlay {
-    objc_setAssociatedObject(self, &overlayKey, overlay, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
-}
-
-- (void)br_setBackgroundColor:(UIColor *)backgroundColor {
-    if (!self.overlay) {
-        [self setBackgroundImage:[[UIImage alloc]init] forBarMetrics:UIBarMetricsDefault];
-        self.overlay = [[UIView alloc] initWithFrame:CGRectMake(0, 0, CGRectGetWidth(self.bounds), CGRectGetHeight(self.bounds) + 20)];
-        self.overlay.userInteractionEnabled = NO;
-        self.overlay.autoresizingMask = UIViewAutoresizingFlexibleWidth;
-        [[self.subviews firstObject] insertSubview:self.overlay atIndex:0];
-    }
-    self.overlay.backgroundColor = backgroundColor;
-}
-
-- (void)br_setTranslationY:(CGFloat)translationY {
-    self.transform = CGAffineTransformMakeTranslation(0, translationY);
-}
-
-- (void)br_setElementsAlpha:(CGFloat)alpha {
-    [[self valueForKey:@"_leftViews"] enumerateObjectsUsingBlock:^(UIView *view, NSUInteger i, BOOL *stop) {
-        view.alpha = alpha;
-    }];
-    [[self valueForKey:@"_rightViews"] enumerateObjectsUsingBlock:^(UIView *view, NSUInteger i, BOOL *stop) {
-        view.alpha = alpha;
-    }];
-    UIView *titleView = [self valueForKey:@"_titleView"];
-    titleView.alpha = alpha;
-    // when viewController first load, the titleView maybe nil
-    [[self subviews] enumerateObjectsUsingBlock:^(UIView *obj, NSUInteger idx, BOOL *stop) {
-        if ([obj isKindOfClass:NSClassFromString(@"UINavigationItemView")]) {
-            obj.alpha = alpha;
-            *stop = YES;
-        }
-    }];
-}
-
-- (void)br_reset {
-    [self setBackgroundImage:nil forBarMetrics:UIBarMetricsDefault];
-    [self.overlay removeFromSuperview];
-    self.overlay = nil;
-}
+//- (UIView *)overlay {
+//    return objc_getAssociatedObject(self, &overlayKey);
+//}
+//
+//- (void)setOverlay:(UIView *)overlay {
+//    objc_setAssociatedObject(self, &overlayKey, overlay, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+//}
+//
+//- (void)br_setBackgroundColor:(UIColor *)backgroundColor {
+//    if (!self.overlay) {
+//        [self setBackgroundImage:[[UIImage alloc]init] forBarMetrics:UIBarMetricsDefault];
+//        self.overlay = [[UIView alloc] initWithFrame:CGRectMake(0, 0, CGRectGetWidth(self.bounds), CGRectGetHeight(self.bounds) + 20)];
+//        self.overlay.userInteractionEnabled = NO;
+//        self.overlay.autoresizingMask = UIViewAutoresizingFlexibleWidth;
+//        [[self.subviews firstObject] insertSubview:self.overlay atIndex:0];
+//    }
+//    self.overlay.backgroundColor = backgroundColor;
+//}
+//
+//- (void)br_setTranslationY:(CGFloat)translationY {
+//    self.transform = CGAffineTransformMakeTranslation(0, translationY);
+//}
+//
+//- (void)br_setElementsAlpha:(CGFloat)alpha {
+//    [[self valueForKey:@"_leftViews"] enumerateObjectsUsingBlock:^(UIView *view, NSUInteger i, BOOL *stop) {
+//        view.alpha = alpha;
+//    }];
+//    [[self valueForKey:@"_rightViews"] enumerateObjectsUsingBlock:^(UIView *view, NSUInteger i, BOOL *stop) {
+//        view.alpha = alpha;
+//    }];
+//    UIView *titleView = [self valueForKey:@"_titleView"];
+//    titleView.alpha = alpha;
+//    // when viewController first load, the titleView maybe nil
+//    [[self subviews] enumerateObjectsUsingBlock:^(UIView *obj, NSUInteger idx, BOOL *stop) {
+//        if ([obj isKindOfClass:NSClassFromString(@"UINavigationItemView")]) {
+//            obj.alpha = alpha;
+//            *stop = YES;
+//        }
+//    }];
+//}
+//
+//- (void)br_reset {
+//    [self setBackgroundImage:nil forBarMetrics:UIBarMetricsDefault];
+//    [self.overlay removeFromSuperview];
+//    self.overlay = nil;
+//}
 
 @end
