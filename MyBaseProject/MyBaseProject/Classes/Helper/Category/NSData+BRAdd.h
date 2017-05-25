da836ff16eea36529833c3a4bd62af0beee95431
任波 <borenfocus@gmail.com>
2017-05-25 19:46:51 +0800

    添加获取当前时间字符串方法

--- MyBaseProject/MyBaseProject/Classes/Helper/Category/NSData+BRAdd.h
+++ MyBaseProject/MyBaseProject/Classes/Helper/Category/NSData+BRAdd.h
@@ -13,4 +13,16 @@
 /** 获取系统当前的时间戳，即当前时间距1970的秒数（以毫秒为单位） */
 + (NSString *)currentTimestamp;
 
+/** 获取当前的时间 */
++ (NSString *)currentDateString;
+
+/**
+ *  按指定格式获取当前的时间
+ *
+ *  @param  formatterStr  设置格式：yyyy-MM-dd HH:mm:ss
+ */
++ (NSString *)currentDateStringWithFormat:(NSString *)formatterStr;
+
+
+
 @end
--- MyBaseProject/MyBaseProject/Classes/Helper/Category/NSData+BRAdd.m
+++ MyBaseProject/MyBaseProject/Classes/Helper/Category/NSData+BRAdd.m
@@ -20,4 +20,23 @@
     return timeString;
 }
 
+#pragma mark - 获取当前的时间
++ (NSString *)currentDateString {
+    return [self currentDateStringWithFormat:@"yyyy-MM-dd HH:mm:ss"];
+}
+
+#pragma mark - 按指定格式获取当前的时间
++ (NSString *)currentDateStringWithFormat:(NSString *)formatterStr {
+    // 获取系统当前时间
+    NSDate *currentDate = [NSDate date];
+    // 用于格式化NSDate对象
+    NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+    // 设置格式：yyyy-MM-dd HH:mm:ss
+    formatter.dateFormat = formatterStr;
+    // 将 NSDate 按 formatter格式 转成 NSString
+    NSString *currentDateStr = [formatter stringFromDate:currentDate];
+    // 输出currentDateStr
+    return currentDateStr;
+}
+
 @end
