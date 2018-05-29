744215468d8b3b80db7ca764de6dd400c047efe7
任波 <borenfocus@gmail.com>
2018-05-29 22:48:36 +0800

    添加导航控制器

---
+++ MyBaseProject/Base/BRNavigationController/BRNavigationController.h
@@ -0,0 +1,13 @@
+//
+//  BRNavigationController.h
+//  MyBaseProject
+//
+//  Created by 任波 on 2018/5/29.
+//  Copyright © 2018年 91renb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BRNavigationController : UINavigationController
+
+@end
